bb965069509e230e4911b71d93de0409addb07da
Aamir Raza <merciful9795@gmail.com>
2024-04-28 01:42:32 +0530

    minor, deploying

--- server/index.js
+++ server/index.js
@@ -5,21 +5,36 @@ import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({ origin: ["http://localhost:5173"] })
+);
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "Hello from DALL.E!",
-  });
+
+// serving frontend build
+
+const frontendDistPath = join(__dirname, "./dist"); // Use join from path
+app.use(express.static(frontendDistPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(frontendDistPath, "index.html"));
 });
+
 
 const startServer = async () => {
   try {
